0463a51281e83337b8204c4362a4948d5352065d
Gerardo <gerardorodore@gmail.com>
2018-03-07 13:00:44 -0600

    Modified init file to load properly for Solairze and tmux to incorporate new customizations

--- dotfiles/init.el
+++ dotfiles/init.el
@@ -15,14 +15,15 @@
 
 ;;========================================================================
 ;; theme
-(add-hook 'after-init-hook (lambda () (load-theme 'misterioso)))
-(add-to-list 'load-path "~/Repos/solarize-emacs")
-;;(load-theme 'solarized-dark t)
-;;(set-frame-parameter nil 'background-mode 'dark)
+;; not really sure what ended up working but also did package-install: color-theme-solarized
+(load-theme 'solarized t) ;; loads solarized theme
+(set-terminal-parameter nil 'background-mode 'dark) ;; dark background
+(set-face-attribute 'font-lock-comment-face nil :foreground "brightred") ;;changes the comment color
+(set-face-italic 'font-lock-comment-face nil) ;; eliminates italic of the comments
 
 
 ;;========================================================================
-;; line numbers
+;; Line numbers
 (global-linum-mode t)
 (setq linum-format "%3d\u2502")
 
@@ -57,8 +58,9 @@ backup-directory-alist
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(package-selected-packages (quote (multiple-cursors solarized-theme anaconda-mode))))
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(package-selected-packages (quote (multiple-cursors solarized-theme anaconda-mode)))
+ '(solarized-italic nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
