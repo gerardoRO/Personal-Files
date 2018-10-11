a7158127bd737733d8489abb4ebfdc2149c4ef2b
Gerardo Rodriguez <rodr0283@umn.edu>
2018-10-11 16:26:33 -0500

    modified init.el for simplicity; added .minttyrc

--- dotfiles/init.el
+++ dotfiles/init.el
@@ -1,24 +1,22 @@
 (require 'package)
+
 (package-initialize)
 ;; load emacs 24 package system. Add MELP repository
 (add-to-list 'package-archives
 	     '("melpa" .
 	       "http://melpa.milkbox.net/packages/") t)
 
+(defvar prelude-packages
+  '(python solarized-theme matlab-mode multiple-cursors))
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+(defun prelude-packages-installed-p ()
+  (loop for p in prelude-packages
+	when (not (packages-installed-p p)) do (return nil)
+	finally (return t)))
 
-
-;;========================================================================
-;; theme
-;; not really sure what ended up working but also did package-install: color-theme-solarized
-(load-theme 'solarized t) ;; loads solarized theme
-(set-terminal-parameter nil 'background-mode 'dark) ;; dark background
-(set-face-attribute 'font-lock-comment-face nil :foreground "brightred") ;;changes the comment color
-(set-face-italic 'font-lock-comment-face nil) ;; eliminates italic of the comments
+(dolist (p prelude-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
 
 
 ;;========================================================================
@@ -44,12 +42,6 @@ backup-directory-alist
 (global-set-key (kbd "C-c a") 'mc/mark-all-like-this) ;; add multiple cursors to all like this
 
 ;;========================================================================
-
-;; ;;autosave
-;;(setq auto-save-file-name-transforms
-      ;;'((".*",temporary-file-directory t)))
-
-;;========================================================================
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -60,7 +52,7 @@ backup-directory-alist
     ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(package-selected-packages
    (quote
-    (airplay matlab-mode multiple-cursors solarized-theme anaconda-mode)))
+    (## airplay matlab-mode multiple-cursors solarized-theme anaconda-mode)))
  '(solarized-italic nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
