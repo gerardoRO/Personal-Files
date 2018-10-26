faefc10210a94dfd7061b675e13f0d57544b1cbc
Gerardo Workstation <rodr0283@umn.edu>
2018-10-26 10:30:34 -0500

    minor bug

--- dotfiles/init.el
+++ dotfiles/init.el
@@ -42,3 +42,17 @@
 (global-set-key (kbd "C-S-a") 'mc/mark-next-word-like-this) ;; add multiple cursors at whole words
 
 ;;========================================================================
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (anaconda-mode solarized-theme multiple-cursors matlab-mode color-theme-solarized airplay))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
